73f20e6ed47b9337aa000c0f502b2db6345db1c1
Alexander Kedrik <alexander.kedrik@gmail.com>
2014-02-11 18:46:35 +0400

    major refactoring

--- parser.d
+++ parser.d
@@ -24,55 +24,86 @@ public class Module {
 
 public Module parse(const(dchar)[] text) {
 	text ~= 0;
-	text ~= 0;
 
 	auto result = new Module;
 
-	TextPosition positionBeforeThat;
-	TextPosition previousPosition;
-	TextPosition position;
 
-	bool isEOF() { return position.index >= text.length-1; }
 
-	dchar currentChar() { return text[position.index]; }
-	dchar previousChar() { return text[previousPosition.index]; }
+	void advancePositionNoSkip(ref TextPosition position) {
+		auto current = text[position.index];
+		if ((current == '\u0000') || (current == '\u001A')) return;
+		auto next = text[position.index+1];
 
-	dchar advance() {
-		if (isEOF()) return 0;
-
-		positionBeforeThat = previousPosition;
-		previousPosition = position;
-
-		if (((currentChar == '\u000D') && (text[position.index+1] != '\u000A')) ||
-		    (currentChar == '\u000A') || (currentChar == '\u2028') || (currentChar == '\u2028')) {
+		if (((current == '\u000D') && (next != '\u000A')) ||
+		    (current == '\u000A') || (current == '\u2028') || (current == '\u2028')) {
 			position.line++;
-			position.column = 1;
-		} else {
-			position.column++;
+			position.column = 0;
 		}
-
-		return text[++position.index];
+		position.column++;
+		position.index++;
 	}
 
-	dchar getCurrentCharAndAdvance() {
-		auto result = currentChar();
-		advance();
-		return result;
+	void skipCrapForPosition(ref TextPosition position) {
+		while ((text[position.index] == '\u000D') || (text[position.index] == '\u000A') ||
+		       (text[position.index] == '\u2028') || (text[position.index] == '\u2029') ||
+		       (text[position.index] == '\u0020') || (text[position.index] == '\u0009') ||
+		       (text[position.index] == '\u000B') || (text[position.index] == '\u000C')) {
+			advancePositionNoSkip(position);
+		}
 	}
 
-	void goToPreviousChar() {
-		position = previousPosition;
-		previousPosition = positionBeforeThat;
+	void advancePosition(ref TextPosition position) {
+		advancePositionNoSkip(position);
+		skipCrapForPosition(position);
 	}
 
+	TextPosition previousPosition;
+	TextPosition currentPosition;
+	TextPosition nextPosition;
 
-	TextRange[] textRangeStack;
+	dchar previousChar() { return text[previousPosition.index]; }
+	dchar  currentChar() { return text[ currentPosition.index]; }
+	dchar     nextChar() { return text[    nextPosition.index]; }
 
+	bool isEOF() { return (currentChar == '\u0000') || (currentChar == '\u001A'); }
+
+	dchar previousCharNoSkip() {
+		if (currentPosition.index == 0) return 0;
+		return text[currentPosition.index-1];
+	}
+
+	dchar nextCharNoSkip() {
+		if (isEOF()) return 0;
+		return text[currentPosition.index+1];
+	}
+
+	void advance() {
+		previousPosition = currentPosition;
+		currentPosition = nextPosition;
+
+		//writeln("new current char ", cast(int)currentChar);
+
+		advancePosition(nextPosition);
+	}
+
+	void advanceNoSkip() {
+		previousPosition = currentPosition;
+		advancePositionNoSkip(currentPosition);
+		if (currentPosition == nextPosition) advancePosition(nextPosition);
+	}
+
+	void skipCrap() {
+		skipCrapForPosition(currentPosition);
+		nextPosition = currentPosition;
+		advancePosition(nextPosition);
+	}
+
+/*
 	// new range starts with current char
 	void startTextRange() {
-		//writeln(std.array.replicate("  ", textRangeStack.length), __FUNCTION__, " ", position);
+		//writeln(std.array.replicate("  ", textRangeStack.length), __FUNCTION__, " ", currentPosition);
 		TextRange textRange;
-		textRange.begin = position;
+		textRange.begin = currentPosition;
 		textRangeStack ~= textRange;
 	}
 
@@ -83,8 +114,8 @@ public Module parse(const(dchar)[] text) {
 
 	// range starts with current char
 	void restartTextRange() {
-		//writeln(std.array.replicate("  ", textRangeStack.length-1), __FUNCTION__, " ", position);
-		currentTextRange().begin = position;
+		//writeln(std.array.replicate("  ", textRangeStack.length-1), __FUNCTION__, " ", currentPosition);
+		currentTextRange().begin = currentPosition;
 	}
 
 	// range starts with previous char
@@ -94,144 +125,53 @@ public Module parse(const(dchar)[] text) {
 
 	// range ends with previous char
 	TextRange endTextRange() {
-		//writeln(std.array.replicate("  ", textRangeStack.length-1), __FUNCTION__, " ", position);
+		//writeln(std.array.replicate("  ", textRangeStack.length-1), __FUNCTION__, " ", currentPosition);
 		TextRange result = currentTextRange();
 		textRangeStack = textRangeStack[0..$-1];
-		result.end = position;
+		result.end = currentPosition;
 		result.text = text[result.begin.index .. result.end.index];
 		return result;
 	}
-
-
-	immutable auto digits = "0123456789"d;
-	immutable auto identifierFirstChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"d;
-	immutable auto identifierChars = identifierFirstChars ~ digits;
-	immutable auto whitespaceChars = "\u0020\u0009\u000B\u000C"d;
-	immutable auto lineBreakChars = "\u000D\u000A\u2028\u2029"d;
-
+*/
 
 
 	struct ParserGenerator {
 		ParserGenerator[dchar] rules;
-		string action;
-		string whilePrefix;
+		string action = "";
 
-		this(string whileLabel = null) {
-			this.whilePrefix = whileLabel ~ ":";
-		}
-
-		ParserGenerator oneOfChars(const dchar[] chars, string action) {
-			return add([chars], action);
-		}
-
-		ParserGenerator oneOfChars(dchar c, string action) {
-			return oneOfChars([c], action);
-		}
-
-		ParserGenerator charSequence(const dchar[] chars, string action) {
-			dchar[][] sequence;
-			foreach (c; chars) sequence ~= [c];
-			return add(sequence, action);
-		}
-
-		ParserGenerator keyword(const dchar[] chars, string actionOnMatch, string actionOnMismatch) {
-			return charSequence(chars,
-				"if(isIdentifierChar(currentChar())){" ~
-					actionOnMismatch ~
-				"}else{" ~
-					actionOnMatch ~
-				"}"
-			);
-		}
-
-		ParserGenerator identifier(string action) {
-			return oneOfChars(identifierFirstChars, "startTextRange(); restartTextRangeIncludingPreviousChar(); while(isIdentifierChar(advance())){}" ~ action);
-		}
-
-		ParserGenerator identifierThatCanStartWithDigit(string action) {
-			return oneOfChars(identifierChars, "startTextRange(); restartTextRangeIncludingPreviousChar(); while(isIdentifierChar(advance())){}" ~ action);
-		}
-
-		ParserGenerator noMatch(string action) {
-			assert(this.action is null);
-			this.action = action;
+		ParserGenerator onCharSequence(const dchar[] chars, string action) {
+			if (chars.length > 0) {
+				if (chars[0] !in rules) rules[chars[0]] = ParserGenerator();
+				rules[chars[0]].onCharSequence(chars[1..$], action);
+			} else {
+				this.action ~= action;
+			}
 			return this;
 		}
 
-		ParserGenerator skipWhitespace() {
-			return oneOfChars(whitespaceChars, "restartTextRange();");
+		ParserGenerator onKeyword(const dchar[] chars, string actionOnMatch, string actionOnMismatch) {
+			return onCharSequence(chars,
+				"if(isIdentifierChar(currentChar())){" ~ actionOnMismatch ~ "}else{" ~ actionOnMatch ~ "}"
+			);
 		}
 
-		ParserGenerator ignoreWhitespace() {
-			return oneOfChars(whitespaceChars, " ");
-		}
-
-		ParserGenerator skipLineBreaks() {
-			return oneOfChars(lineBreakChars, "restartTextRange();");
-		}
-
-		ParserGenerator ignoreLineBreaks() {
-			return oneOfChars(lineBreakChars, " ");
-		}
-
-		ParserGenerator handleComments() {
-			return charSequence("//", "finishParsingLineComment();")
-			      .charSequence("/*", "finishParsingBlockComment();")
-			      .charSequence("/+", "finishParsingNestingBlockComment();");
-		}
-
-
-		string generate() const {
-			return whilePrefix ~ "while(true){" ~ code ~ "}";
-		}
-
-		//key - array of possible values for chars
-		//key[i] - array of possible values for char #i
-		//key[i][j] - one of possible values for char #i
-		private ParserGenerator add(const(dchar[][]) key, string action) {
-			if (key.length > 0) {
-				foreach (c; key[0]) {
-					if (c !in rules) rules[c] = ParserGenerator();
-					rules[c].add(key[1..$], action);
-				}
-			} else {
-				assert(this.action is null);
-				this.action = action;
-			}
+		ParserGenerator onNoMatch(string action) {
+			this.action ~= action;
 			return this;
 		}
 
 		private string code() const {
 			if (rules.length == 0) return action;
-			string result = "switch(getCurrentCharAndAdvance()){";
-
-			dchar[][string] codeToCharsMap;
+			auto result = "{switch(currentChar()){";
 			foreach (key, value; rules) {
-				auto generatedCode = value.code;
-				if (generatedCode in codeToCharsMap) {
-					codeToCharsMap[generatedCode] ~= key;
-				} else {
-					codeToCharsMap[generatedCode] = [key];
-				}
+				result ~= format(`case'\U%08X':{advanceNoSkip();%s}break;`, key, value.code);
 			}
-
-			foreach (generatedCode, chars; codeToCharsMap) {
-				foreach (c; chars) result ~= format(`case'\U%08X':`, c);
-				result ~= "{" ~ generatedCode ~ "}break;";
-			}
-
-			if (action !is null) {
-				result ~= format(`default:{%s}break;}`, action);
-			} else {
-				result ~= `default:}`;
-			}
-
-			return result;
+			return result ~ format(`default:{advanceNoSkip();%s}break;}}`, action);
 		}
 	}
 
 	template generateParser(rulesTuple...) {
-		immutable string generateParser = rulesTuple[0].generate();
+		immutable string generateParser = rulesTuple[0].code;
 	}
 
 
@@ -243,15 +183,43 @@ public Module parse(const(dchar)[] text) {
 		return (c == '\u0020') || (c == '\u0009') || (c == '\u000B') || (c == '\u000C');
 	}
 
-	void error(dstring message, TextRange textRange = TextRange(previousPosition, position)) {
-		if (isEOF()) {
-			textRange.end = previousPosition;
+	void error(string message, TextRange textRange = TextRange(previousPosition, currentPosition)) {
+		if (textRange.end.index >= text.length-1) {
+			assert(isEOF);
+			textRange.end = currentPosition;
 		}
 		textRange.text = text[textRange.begin.index .. textRange.end.index];
 		auto error = new ParseError;
-		error.message = std.conv.text(message);
+		error.message = message;
 		error.textRange = textRange;
 		result.errors ~= error;
+	}
+
+	void errorExpected(string message) {
+		TextRange textRange = TextRange(currentPosition, currentPosition);
+		if (currentChar == 0) {
+			error(format("expected " ~ message ~ ", found end of file"), textRange);
+		} else {
+			advancePositionNoSkip(textRange.end);
+			error(format("expected " ~ message ~ ", found '%c'", currentChar), textRange);
+		}
+	}
+
+	void errorExpectedChars(dchar[] chars) {
+		assert(!chars.empty);
+		string message;
+		if (chars.length == 1) {
+			message = format("'%c'", chars[0]);
+		} else if (chars.length == 2) {
+			message = format("'%c' or '%c'", chars[0], chars[1]);
+		} else if (chars.length == 3) {
+			foreach (c; chars[0..$-2]) {
+				message = format("'%c', ", c);
+			}
+			message ~= format("'%c' or '%c'", chars[0], chars[1], chars[2]);
+		}
+
+		errorExpected(message);
 	}
 
 
@@ -265,112 +233,55 @@ public Module parse(const(dchar)[] text) {
 		assert(0);
 	}
 
-	void finishParsingIdentifier() {
-		while (isIdentifierChar(advance())) {}
+	TextRange parseIdentifier() {
+		skipCrap();
+		//writeln(__FUNCTION__, " ", currentChar);
+		TextRange result;
+		result.begin = currentPosition;
+		while (isIdentifierChar(currentChar)) {  // TODO: can't start with digit
+			advanceNoSkip();
+		}
+		result.end = currentPosition;
+		result.text = text[result.begin.index .. result.end.index];
+		skipCrap();
+		//writeln(__FUNCTION__, " ", result.text);
+		return result;
 	}
 
-	auto finishParsingModuleDeclaration()
+	bool parseChar(dchar c) {
+		if (currentChar == c) {
+			advanceNoSkip();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void finishParsingModuleDeclaration(TextPosition begin)
 	in {
 		//assert(moduleKeywordTextRange.text == "module");
-		//assert(position == moduleKeywordTextRange.firstCharIndex + moduleKeywordTextRange.text.length);
+		//assert(currentPosition == moduleKeywordTextRange.firstCharIndex + moduleKeywordTextRange.text.length);
 	}
 	body {
 		auto d = new ModuleDeclaration;
+		d.textRange.begin = begin;
+		d.textRange.end = currentPosition;
 		result.declarations ~= d;
-		TextPosition moduleKeywordEnd = position;
 
-		//mixin(generateParser!(ParserGenerator("P1").ignoreWhitespace().ignoreLineBreaks().handleComments()
-		//	.identifierThatCanStartWithDigit(
-		//		"d.packageNames ~= endTextRange();" ~
-		//		ParserGenerator("P2").ignoreWhitespace().ignoreLineBreaks().handleComments()
-		//			.oneOfChars(['.'], "break P2;")
-		//			.oneOfChars([';'], "d.textRange = endTextRange(); break P1;")
-		//			.oneOfChars([0], "error(`expected '.' or ';', found end of file`);         goToPreviousChar(); d.textRange = endTextRange(); if (!d.packageNames.empty) { d.textRange.end = d.packageNames[$-1].end; } else { d.textRange.end = moduleKeywordEnd; } break P1;")
-		//			.noMatch(        "error(`expected '.' or ';', found '`d~previousChar~`'`); goToPreviousChar(); d.textRange = endTextRange(); if (!d.packageNames.empty) { d.textRange.end = d.packageNames[$-1].end; } else { d.textRange.end = moduleKeywordEnd; } break P1;")
-		//			.generate()
-		//	)
-		//	.oneOfChars(['.'], "startTextRange(); d.packageNames ~= endTextRange();")
-		//	.oneOfChars([';'], "startTextRange(); d.packageNames ~= endTextRange(); d.textRange = endTextRange(); break P1;")
-		//	.oneOfChars([0], "error(`expected package name, found end of file`);         goToPreviousChar(); d.textRange = endTextRange(); if (!d.packageNames.empty) { d.textRange.end = d.packageNames[$-1].end; } else { d.textRange.end = moduleKeywordEnd; } break P1;")
-		//	.noMatch(        "error(`expected package name, found '`d~previousChar~`'`); goToPreviousChar(); d.textRange = endTextRange(); if (!d.packageNames.empty) { d.textRange.end = d.packageNames[$-1].end; } else { d.textRange.end = moduleKeywordEnd; } break P1;")
-		//));
+		do {
+			d.packageNames ~= parseIdentifier();
+		} while (parseChar('.'));
 
-
-		TextRange createTextRange(TextPosition begin, TextPosition end) {
-			TextRange result;
-			result.begin = begin;
-			result.end = end;
-			result.text = text[begin.index .. end.index];
-			return result;
-		}
-
-		TextRange emptyRangeStartingFromPreviousChar() {
-			return createTextRange(previousPosition, previousPosition);
-		}
-
-		TextRange parseIdentifierStartingFromPreviousChar() {
-			TextPosition begin = previousPosition;
-			while (isIdentifierChar(advance())) {}
-			return createTextRange(begin, position);
-		}
-
-		void errorExpected(dstring expected) {
-			if (previousChar == 0) {
-				error(dtext(format("expected %s, found end of file", expected)));
-			} else {
-				error(dtext(format("expected %s, found '%c'", expected, previousChar)));
+		if (parseChar(';')) {
+			d.textRange.end = currentPosition;
+		} else {
+			errorExpectedChars(['.', ';']);
+			if (!d.name.empty) {
+				d.textRange.end = d.packageNames[$-1].end;
 			}
 		}
 
-		bool shouldSkip(dchar c) {
-			switch (c) {
-				case'\U00000020':  // FIXME: move to advance()
-				case'\U0000000C':
-				case'\U00002028':
-				case'\U00000009':
-				case'\U0000000D':
-				case'\U00002029':
-				case'\U0000000A':
-				case'\U0000000B':
-					return true;
-				default:
-					return false;
-			}
-		}
-
-		dchar advanceSkipping() {
-			do {
-				advance();
-			} while (shouldSkip(previousChar));
-			return previousChar;
-		}
-
-		while (true) {
-			advanceSkipping();
-			if (isIdentifierChar(previousChar)) {
-				d.packageNames ~= parseIdentifierStartingFromPreviousChar();
-				advanceSkipping();
-				if (previousChar != '.') {
-					if (previousChar != ';') {
-						errorExpected("'.' or ';'");
-						goToPreviousChar();
-					}
-					break;
-				}
-			} else if ((previousChar == '.') || (previousChar == ';')) {
-				d.packageNames ~= emptyRangeStartingFromPreviousChar();
-			} else {
-				errorExpected("package name");
-				goToPreviousChar();
-			}
-			if (!isIdentifierChar(previousChar) && (previousChar != '.')) break;
-		}
-
-		d.textRange = endTextRange();
-		if (previousChar() != ';') {
-			d.textRange.end = d.packageNames.empty ? moduleKeywordEnd : d.packageNames[$-1].end;
-			d.textRange.text = text[d.textRange.begin.index .. d.textRange.end.index];
-		}
+		d.textRange.text = text[d.textRange.begin.index .. d.textRange.end.index];
 
 		if (d.name.empty) {
 			error(`no module name`, d.textRange);
@@ -381,8 +292,8 @@ public Module parse(const(dchar)[] text) {
 		}
 	}// Todo missing dot test
 
-	void finishParsingImportDeclaration() {
-		void parseImportList(TextRange textRange) {
+	void finishParsingImportDeclaration(TextPosition begin) {
+		/*void parseImportList(TextRange textRange) {
 			assert(0);
 		}
 
@@ -390,20 +301,25 @@ public Module parse(const(dchar)[] text) {
 		mixin(generateParser!(ParserGenerator().ignoreWhitespace().ignoreLineBreaks().handleComments()
 			.oneOfChars(identifierChars, "return parseImportList(endTextRange());")
 			//.noMatch
-		));
+		));*/
 	}
+
+	//writeln(ParserGenerator()
+	//		.onKeyword("module", "return finishParsingModuleDeclaration();", "return finishParsingIdentifier();")
+	//		.onKeyword("import", "return finishParsingImportDeclaration();", "return finishParsingIdentifier();")
+	//		.onNoMatch("/*error(`expected declaration`);*/ writeln(`onNoMatch`) return;").code);
 
 	void parseDeclaration() {
-		startTextRange();
-		mixin(generateParser!(ParserGenerator().skipWhitespace().skipLineBreaks().handleComments()
-			.keyword("module", "return finishParsingModuleDeclaration();", "return finishParsingIdentifier();")
-			.keyword("import", "return finishParsingImportDeclaration();", "return finishParsingIdentifier();")
-			.oneOfChars([0], "return;")
-			.noMatch("/*error(`expected declaration`);*/ return;")
+		skipCrap();
+		TextPosition begin = currentPosition;
+		mixin(generateParser!(ParserGenerator()
+			.onKeyword("module", "return finishParsingModuleDeclaration(begin);", "")
+			.onKeyword("import", "return finishParsingImportDeclaration(begin);", "")
+			.onNoMatch("/*error(`expected declaration`);*/ return;")
 		));
 	}
 
-
+	//writeln("initial nextPosition.index: ", nextPosition.index);
 	while (!isEOF()) {
 		parseDeclaration();
 	}
